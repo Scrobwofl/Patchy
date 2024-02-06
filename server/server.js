cd9385f108e7d83f591ac2a0a76ebd18ef0af810
gabaal <geraldbaalham@live.co.uk>
2024-02-06 10:50:35 +0000

    Working GET, POST and DELETE, still need to get PUT working

--- server/server.js
+++ server/server.js
@@ -45,13 +45,26 @@ app.delete('/plants/:id', (req, res) => {
   }
 })
 
-// Post new plant
+// TESTING Post new plant
+// app.post("/plants", (req, res) =>{
+//   console.log(req.body)
+//   try {
+//     const API_ID = req.body.API_ID;
+//     const name = req.body.name;
+//     const newPlant = db.prepare(`INSERT INTO plants (API_ID, name) VALUES (?,?)`).run(API_ID, name);
+//     response.json(newPlant);
+//   } catch (err) {
+//     res.status(500).json({ error: err})
+//   }
+// })
+
+
 app.post("/plants", function (req, res) {
   console.log(req.body);
   try{
     const API_ID = req.body.API_ID
     const name = req.body.name;
-    const en_wikipedia_url = req.body.en_wikipedia_url;
+    const en_wikipedia_url = req.body.en_wikipedia_url
     const height = req.body.height
     const spread = req.body.spread
     const description = req.body.description
@@ -61,12 +74,43 @@ app.post("/plants", function (req, res) {
     const sun_requirements = req.body.sun_requirements
     const scientific_names = req.body.scientific_names
     const patches = req.body.patches
-    const newPlant = db
-    .prepare(`INSERT INTO plants (API_ID,name, en_wikipedia_url, height, spread, description, row_spacing, sowing_method, main_image_path, sun_requirements, scientific_names, patches) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`)
-    .run(plant);
+    const newPlant = db.prepare(`INSERT INTO plants (API_ID, name, en_wikipedia_url, height, spread, description, row_spacing, sowing_method, main_image_path, sun_requirements, scientific_names, patches) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`)
+    .run(API_ID, name, en_wikipedia_url, height, spread, description, row_spacing, sowing_method, main_image_path, sun_requirements, scientific_names, patches);
   response.json(newPlant);
   } catch (err) {
     res.status(500).json({ error: err})
   }
 });
 
+//TESTING PUT to update a plant
+
+
+app.put("plants/:id", (req, res) => {
+  console.log(req.params, req.body);
+   try {
+    const id = req.params.id;
+    const API_ID = req.body.API_ID
+    const name = req.body.name;
+    const en_wikipedia_url = req.body.en_wikipedia_url
+    const height = req.body.height
+    const spread = req.body.spread
+    const description = req.body.description
+    const row_spacing = req.body.row_spacing
+    const sowing_method = req.body.sowing_method
+    const main_image_path = req.body.main_image_path
+    const sun_requirements = req.body.sun_requirements
+    const scientific_names = req.body.scientific_names
+    const patches = req.body.patches
+
+    const updatePlant = db
+      .prepare(
+        `UPDATE plants SET API_ID = ?, name = ?, en_wikipedia_url = ?, height = ?, spread = ?, description = ?, row_spacing = ?, sowing_method = ?, main_image_path = ?, sun_requirements = ?, scientific_names = ?, patches = ?`
+        )
+        .run(API_ID, name, en_wikipedia_url, height, spread, description, row_spacing, sowing_method, main_image_path, sun_requirements, scientific_names, patches, id)
+    res.status(204).json({amended: updatePlant})
+   } catch (err) {
+    console.log("failed the try")
+    res.status(500).json({ error: err });
+  }
+}
+)
